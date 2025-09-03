c414899dbf512c0c393a1695baf970e39692618d
Your Name <gaurav.webyug@gmail.com>
2025-09-03 15:22:08 +0530

    frontend + backend setup

--- student-backend/server.js
+++ student-backend/server.js
@@ -66,4 +66,5 @@ app.delete("/students/:id", async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
